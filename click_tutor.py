eb7f9199e8151b4c6ee667ea2c476cf6dc99a6ba
Laptop <@>
2024-07-31 23:52:12 +0300

    home

---
+++ click_tutor.py
@@ -0,0 +1,20 @@
+from time import sleep
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+driver.get('https://www.freeconferencecall.com/global/ua')
+
+login_button = driver.find_element('xpath', '//a[@id="login-desktop"]')
+login_button.click()
+
+email_field = driver.find_element('xpath', '//input[@id="login_email"]')
+email_field.send_keys('ihnatenkobohdan@gmail.com')
+print(email_field.get_attribute('value'))
+email_field.clear()
+
+sleep(5)
